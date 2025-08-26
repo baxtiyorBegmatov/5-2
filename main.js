a3332f0b9a7ad7bc8c673eeca509d51ee8572313
baxtiyorBegmatov <baxtiyorbegmatov204@gmail.com>
2025-08-26 09:25:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const userName = prompt("String");
+
+alert(`${userName} sozi : ${typeof userName}`);
+const hayokiyoq = prompt("Boolean Kiriting");
+
+console.log(`${hayokiyoq} sozi: ${typeof Boolean(Xayot)}`);
+const Qw = prompt("String");
+
+alert(`${Xayot} sozi : ${typeof userName}`);
+const Xayot = prompt("Boolean Kiriting");
+
+console.log(`${Xayot} sozi: ${typeof Boolean(Xayot)}`);
+
++Number('45216789348765267890n');
+console.log(userName);
